5ac012c69bac991b35cf32e0cf68d0fa2f5f1257
cblanco-clintell <c.blanco@clintell.io>
2025-03-17 20:00:53 +0100

    Feat - Django Q Tasks

--- mails/tasks.py
+++ mails/tasks.py
@@ -1,11 +1,15 @@
-# from django.core.management import call_command
-# from django.utils import timezone
+from django.core.management import call_command
 
 
-# def mail_pending_sessions(*args, **kwargs):
-#     """Mail pending sessions task."""
-#     try:
-#         call_command('mail_pending_sessions')
-#     except Exception as e:
-#         print(e)
-#         raise
+def mail_pending_sessions(*args, **kwargs):
+    """Mail pending sessions task."""
+    try:
+        call_command('mail_pending_sessions')
+    except Exception as e:
+        print(e)
+        raise
+
+
+def test_task(*args, **kwargs):
+    """Test task."""
+    print("Test task")
