72ec238ddba261ac2cc6c3d9bfb4349cb431b1da
cblanco-clintell <c.blanco@clintell.io>
2025-08-23 13:51:58 +0200

    Feat - Lead Admin

--- leads/admin.py
+++ leads/admin.py
@@ -6,6 +6,8 @@ from .models import Lead
 
 @admin.register(Lead)
 class LeadAdmin(admin.ModelAdmin):
-    list_display = ['email', 'lead_type', 'unsubscribed', 'created']
-    list_filter = ['lead_type', 'unsubscribed', 'created']
+    list_display = ['email', 'lead_type', 'unsubscribed', 'created', 
+                    'first_name', 'last_name', 'instagram_username', 
+                    'form_code']
+    list_filter = ['lead_type', 'unsubscribed', 'created','form_code']
     search_fields = ['email']
